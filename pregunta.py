f0e15cb47d10da8a13bde2e134e942e2dfa07ae6
melipp <143761518+melipp@users.noreply.github.com>
2023-10-21 00:15:31 -0500

    p1

--- pregunta.py
+++ pregunta.py
@@ -12,6 +12,7 @@ import pandas as pd
 def clean_data():
     
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    
     df.rename(columns = {'Unnamed: 0': 'index'}, inplace = True)
     df.set_index('index', inplace = True)
     df.sexo = df.sexo.str.lower()
