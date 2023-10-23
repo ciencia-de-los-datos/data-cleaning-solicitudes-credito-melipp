24b8891cf7d269605a0697ca26c5a7bfcd4a39c4
melipp <143761518+melipp@users.noreply.github.com>
2023-10-22 21:57:15 -0500

    lab 5

--- pregunta.py
+++ pregunta.py
@@ -13,40 +13,13 @@ def clean_data():
     
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    df.rename(columns = {'Unnamed: 0': 'index'}, inplace = True)
-    df.set_index('index', inplace = True)
+    df.dropna(inplace=True)
 
-    #Columna Sexo
-    df.sexo = df.sexo.str.lower()
-    df.sexo = df.sexo.astype('category')
-
-    #Columna tipo_de_emprendimiento
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.astype('category')
-
-    #Columna idea_negocio
-    df.idea_negocio = df.idea_negocio.str.lower().str.strip('_').str.strip('-').str.strip().str.replace('_',' ').str.replace('-',' ')
-    df.idea_negocio = df.idea_negocio.astype('category')
-
-    #Columna barrio
-    # df.barrio = df.barrio.str.lower().str.strip('_').str.strip('-').str.strip().str.replace('_',' ').str.replace('-',' ')
-    df.barrio = df.barrio.str.lower().str.replace('_','-').str.replace("-", " ")
-
-    #Columna comuna_ciudadano
-    df.comuna_ciudadano = df.comuna_ciudadano.astype('Int64')
-    df.comuna_ciudadano = df.comuna_ciudadano.astype('category')
-
-    #Columna fecha_de_beneficio
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst = True)
-
-    #Columna monto_del_credito
-    df.monto_del_credito = df.monto_del_credito.str.replace('$','').str.replace(',','')
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
     df.monto_del_credito = df.monto_del_credito.astype(float)
 
-    #Columna línea_credito
-    df.línea_credito = df.línea_credito.str.lower().str.strip('_').str.strip('-').str.strip().str.replace('_',' ').str.replace('-',' ')
-
-    df.dropna(inplace = True)
-    df.drop_duplicates(inplace = True)
+    df.drop_duplicates(inplace=True)
     
     return df
